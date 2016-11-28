2293e4c8241f514d22576dfc20e1ffcef4f21900
John <Rocky5@users.noreply.github.com>
2016-11-28 09:28:52 +0000

    Modified UnleashX to remove the V drive, since it only purpose it to show the content of the virtual D drive, and that's pointless since the D dive does that.
    Also removed the other Unleashx xbes and when building the zips use the softmod version for all, bar the extras disc ones.
    ( also removed the MSD installer folder and config as they're no longer used )

--- Build Release.bat
+++ Build Release.bat
@@ -19,14 +19,18 @@ Copy "Tools\thumb.jpg" "..\Release\"
 Move "*.iso" "..\Release\"
 Move "*.zip" "..\Release\"
 
+Copy "%CD%\Save Folder\nkpatcher settings\unleashx.xbe" "%CD%\Variants\Quick Upgrade\Prep\unleashx.xbe"
 XCopy /S /Y "%CD%\Extras Disc\softmod files\Softmod Files" "%CD%\Variants\Quick Upgrade\Prep\Softmod Files\"
+Copy "%CD%\Save Folder\nkpatcher settings\unleashx.xbe" "%CD%\Variants\XBHDM Build\linux\E\Prep\default.xbe"
 XCopy /S /Y "%CD%\Extras Disc\softmod files\Softmod Files" "%CD%\Variants\XBHDM Build\linux\E\Prep\Softmod Files\"
 
 CD "Variants"
 "%Winrar%" a -x*.db -afzip "Quick Upgrade" "Quick Upgrade"
 "%Winrar%" a -x*.db -afzip "XBHDM Build" "XBHDM Build"
 RD /q /s "Quick Upgrade\Prep\Softmod Files"
+Del /q /s "Quick Upgrade\Prep\unleashx.xbe"
 RD /q /s "XBHDM Build\linux\E\Prep\Softmod Files"
+Del /q /s "XBHDM Build\linux\E\Prep\default.xbe"
 
 Move "*.zip" "..\..\Release\"
 
